fdf820f2017befcf6540f0b5deef3fc5dc754267
Beginning0 <39624026+Beginning0@users.noreply.github.com>
2019-09-06 17:28:54 -0400

    bla
    1313

--- clase3/app/src/main/java/com/example/clase3/quien.java
+++ clase3/app/src/main/java/com/example/clase3/quien.java
@@ -1,14 +1,25 @@
 package com.example.clase3;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class quien extends AppCompatActivity {
 
+    private Button botonact2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quien);
+
+        Button btn2 = (Button) findViewById(R.id.botonact2);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent2 = new Intent(v.getContext(), Calculadora.class);
+                startActivityForResult(intent2, 0);
     }
 }
